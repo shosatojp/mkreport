0afef6b0afd68f502f0e138806b3d3ebc8d063b0
shosatojp <me@shosato.jp>
2020-06-30 14:07:00 +0900

    update

--- convert.py
+++ convert.py
@@ -2,12 +2,19 @@ import cv2
 import numpy as np
 import os
 import sys
+import argparse
 
-JPEG_QUALITY = 70
-GRAYSCALE_THRESHOLD = 190
+parser = argparse.ArgumentParser()
+parser.add_argument('-t', '--type', type=str, choices=['grayscale', 'binary'], default=False, required=False)
+parser.add_argument('-q', '--quality', type=int, default=70, help='quality')
+parser.add_argument('-g', '--grayscale_threshold', type=int, default=210, help='grayscale threshold')
+parser.add_argument('-o', '--outtype', type=str, choices=['jpg', 'png'], required=True, help='output image type')
+parser.add_argument('input')
+parser.add_argument('output')
+args = parser.parse_args()
 
-input_filename = sys.argv[1]
-output_filename = sys.argv[2] if len(sys.argv) >= 3 else os.path.splitext(input_filename)[0] + '.jpg'
+input_filename = args.input
+output_filename = args.output if len(sys.argv) >= 3 else os.path.splitext(input_filename)[0] + '.jpg'
 
 
 im = cv2.imread(input_filename, cv2.IMREAD_GRAYSCALE)
@@ -16,13 +23,18 @@ if type(im) == None:
     print('failed to load image', file=sys.stderr)
     exit(1)
 
-im = cv2.medianBlur(im, 3)
-
-im = im + ((im > GRAYSCALE_THRESHOLD) * 255)
-im = im - ((im <= 190) * 255)
-im = np.clip(im, 0, 255)
+if args.type == 'binary':
+    im = cv2.medianBlur(im, 3)
+    im = im + ((im > args.grayscale_threshold) * 255)
+    im = im - ((im <= args.grayscale_threshold) * 255)
+    im = np.clip(im, 0, 255)
 
 im.astype(np.uint8)
-cv2.imwrite(output_filename, im, params=[cv2.IMWRITE_PNG_COMPRESSION, 9])
-# cv2.imwrite(output_filename, im, params=[cv2.IMWRITE_JPEG_QUALITY, JPEG_QUALITY])
+
+if args.outtype == 'jpg':
+    cv2.imwrite(output_filename, im, params=[cv2.IMWRITE_JPEG_QUALITY, args.quality])
+elif args.outtype == 'png':
+    cv2.imwrite(output_filename, im, params=[cv2.IMWRITE_PNG_COMPRESSION, 9])
+
+
 print('output:', output_filename)
